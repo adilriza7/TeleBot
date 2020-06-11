2715272d28e8b3ea98f2238d63592d9ceccbc83b
adilriza7 <66129233+adilriza7@users.noreply.github.com>
2020-06-11 06:39:46 +0530

    Add files via upload

--- userbot/plugins/quotly.py
+++ userbot/plugins/quotly.py
@@ -1,12 +1,13 @@
 """QuotLy: Avaible commands: .qbot
 """
 import datetime
+import asyncio
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd(pattern="qbot ?(.*)"))
+@borg.on(admin_cmd(pattern="qbot ?(.*)",outgoing=True))
 async def _(event):
     if event.fwd_from:
         return 
